156f88fa0fc968649070df788c3a778f3f658f46
Henrique de O. Fadoni <38730725+HenriqueFadoni@users.noreply.github.com>
2018-12-20 14:30:58 -0200

    Creating Dynamic Nested Routes

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -44,6 +44,7 @@ class Posts extends Component {
                     <Post
                     title={post.title}
                     author={post.author}
+                    key={post.id}
                     clicked={() => {
                         this.postSelectedHandler(post.id)
                     }} />
